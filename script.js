a30577dd08d321a1683ee19011025bf9ff77bd2f
Andrew <59698668+andrew-shigabetdinov@users.noreply.github.com>
2020-01-09 18:20:13 +0300

    ДЗ Числа

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// 1-е задание
+var numberOne = (Math.PI).toFixed(2);
+console.log(numberOne);
+
+// 2-е задание
+var numberTwo = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+console.log(numberTwo);
+
+// 3-е задание (A)
+var numberThree = (Math.random()).toFixed(2);
+console.log(numberThree);
+
+// 3-е задание (B)
+
+var numberFour = Math.floor(Math.random() * 100 + 1);
+console.log(numberFour);
+
+// 4-е задание
+
+var numberFive = (0.6*10 + 0.7*10)/10;
+console.log(numberFive);
+
+// 5-е задание
+
+var stringSix = '100$';
+var numberSix = parseInt(stringSix);
+console.log(numberSix);
